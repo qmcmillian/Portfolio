ccb7092c7f0317bee40d73cde26187f3a0cf394d
Quentin McMillian <quentinmcmillian@gmail.com>
2025-01-27 20:15:16 -0600

    Responsive layout fixes
    - Made tech skills 4 columns and 2 on phones.
    - Swapped form orientation on phone screens

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ import working from '../components/main/icons/working.svg';
 const Contact = () => {
   return (
     <section className='bg-violet-200 py-12' id='contact'>
-      <div className='max-w-6xl mx-auto grid md:grid-cols-2 items-center gap-8 px-8'>
+      <div className='max-w-6xl mx-auto flex flex-col-reverse md:grid md:grid-cols-2 items-center gap-8 px-8'>
         <article className="flex justify-center">
           <Form />
         </article>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,14 +6,14 @@ import { skills } from '../data';
 const Skills = () => {
   return (
     <section className='py-20 align-element' id="skills">
-      <SectionTile text='Tech Stack'/>
-      <div className='py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3' >
-        { skills.map((skill) => {
-          return <SkillsCard key={skill.id} { ...skill} />
-        })}
+      <SectionTile text='Tech Stack' />
+      <div className='py-16 grid gap-8 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4' >
+        {skills.map((skill, index) => (
+          <SkillsCard key={index} {...skill} />
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Skills;
