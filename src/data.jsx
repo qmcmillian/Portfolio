f640e54cd8b33106e4ca16513eeee36c23b23d60
Quentin McMillian <quentinmcmillian@gmail.com>
2025-01-30 10:47:05 -0600

    add contacts to nav bar

--- src/data.jsx
+++ src/data.jsx
@@ -6,7 +6,8 @@ export const links = [
 	{ id: nanoid(), href: '#home', text: 'home'},
 	{ id: nanoid(), href: '#skills', text: 'skills'},
 	{ id: nanoid(), href: '#about', text: 'about'},
-	{ id: nanoid(), href: '#projects', text: 'projects'},
+	// { id: nanoid(), href: '#projects', text: 'projects'},
+	{ id: nanoid(), href: '#contact', text: 'Contact'},
 ];
 
 export const skills = [
